8ab3e3db35b6c9ffda73b1b77ed5e9cbf25d6d44
Sunil Yadav <aiodsunil@gmail.com>
2019-10-30 14:17:56 +0000

    kafka

--- src/main/kotlin/com/aiod/eps/EventProcessingSvcApplication.kt
+++ src/main/kotlin/com/aiod/eps/EventProcessingSvcApplication.kt
@@ -2,10 +2,16 @@ package com.aiod.eps
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
 
 
 @SpringBootApplication
-class EventProcessingSvcApplication
+class EventProcessingSvcApplication{
+    @Bean
+    fun kotlinFunction(): (String) -> String {
+        return  { it.toUpperCase() }
+    }
+}
 
 fun main(args:Array<String>) {
     runApplication<EventProcessingSvcApplication>(*args)
