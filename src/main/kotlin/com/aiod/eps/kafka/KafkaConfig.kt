7b4b591746d923970ffdaae7cc456fef69c4ff5e
Sunil Yadav <aiodsunil@gmail.com>
2019-10-30 14:50:51 +0000

    kafka

---
+++ src/main/kotlin/com/aiod/eps/kafka/KafkaConfig.kt
@@ -0,0 +1,13 @@
+package com.aiod.eps.kafka
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+//@Configuration
+//class KafkaConfig {
+//
+//   @Bean
+//    fun kotlinFunction(): (String) -> String {
+//        return  { it.toUpperCase() }
+//    }
+//}
